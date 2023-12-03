bc06bd4b8026f6ddf759021acd1ddfc3d662257a
Evan Hu <evanhu@berkeley.edu>
2023-12-03 13:53:39 -0800

    updated photos

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -7,27 +7,24 @@ const Photography = () => {
                 {[
                     "https://i.imgur.com/2d6ahrU.jpg",
                     "https://i.imgur.com/hUjlPXV.jpg",
-                    "https://i.imgur.com/wJo23So.jpg",
                     "https://i.imgur.com/UQ78Skm.jpg",
+                    "https://live.staticflickr.com/65535/53373781774_fa78c4f486_c_d.jpg",
                     "https://i.imgur.com/C3vOSkv.jpg",
                     "https://i.imgur.com/OQAOxhX.jpg",
                     "https://i.imgur.com/9Tva5x6.jpg",
-                    "https://i.imgur.com/Kvf3GRt.jpg",
                     "https://i.imgur.com/pwnntsv.jpg",
                     "https://i.imgur.com/CQwZGC6.jpg",
+                    "https://live.staticflickr.com/65535/53373781794_51ace553ec_c_d.jpg",
                     "https://i.imgur.com/FWdnUg3.jpg",
-                    "https://i.imgur.com/4eRGZYs.jpg",
                     "https://i.imgur.com/p09b7Vx.jpg",
-                    "https://i.imgur.com/EqiEWx9.jpg",
                     "https://i.imgur.com/GAMoq7m.jpg",
                     "https://i.imgur.com/2G49YCv.jpg",
+                    "https://live.staticflickr.com/65535/53372554802_ddcdf675a5_c_d.jpg",
+                    "https://live.staticflickr.com/65535/53373781799_0145e19411_c_d.jpg",
                     "https://i.imgur.com/zCpBUSg.png",
-                    "https://i.imgur.com/boWjTcg.jpg",
                     "https://i.imgur.com/UZH7YMX.jpg",
                     "https://i.imgur.com/cHZFzB7.jpg",
                     "https://i.imgur.com/0zSDZTu.jpg",
-                    "https://i.imgur.com/1M5k0Sy.jpg",
-                    "https://i.imgur.com/lLWaeMf.jpg"
                 ].map((src, index) => (
                     <img key={index} src={src} className="mb-2 w-full h-auto" style={{ breakInside: 'avoid' }} />
                 ))}
